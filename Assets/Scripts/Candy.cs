4ff0ee3db36f3ddf73618d01609aa995c3277ef6
zhuy456z <huylahoai000@gmail.com>
2025-04-15 23:41:16 +0700

    add candy and relation scripts

---
+++ Assets/Scripts/Candy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CandyType
+{
+
+    Red,
+    Yellow,
+    Green,
+    Purple,
+    Blue,
+    Orange
+} 
+public class Candy : MonoBehaviour
+{
+    [SerializeField] public int xIndex;
+    [SerializeField] public int yIndex;
+    [SerializeField] public int currentPosition;
+    [SerializeField] public int targetPosition;
+    [SerializeField] public bool isMoving;
+    [SerializeField] public bool isMatched;
+    [SerializeField] public CandyType candyType;
+    // Start is called before the first frame update
+    public Candy(int xIndex, int yIndex)
+    {
+        this.xIndex = xIndex;
+        this.yIndex = yIndex;
+    }
+}
